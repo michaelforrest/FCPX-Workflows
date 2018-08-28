023fac6905b8bcf6ba07c0f9004d96dd88f98e11
Michael Forrest <michael.forrest@gmail.com>
2018-08-28 14:15:49 +0100

    Initial Commit

---
+++ FCPX-Workflows/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FCPX-Workflows
+//
+//  Created by Michael Forrest on 28/08/2018.
+//  Copyright © 2018 Good To Hear. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FCPX-Workflows/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FCPX-Workflows
+//
+//  Created by Michael Forrest on 28/08/2018.
+//  Copyright © 2018 Good To Hear. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
